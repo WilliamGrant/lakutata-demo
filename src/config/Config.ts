d87736d9c42891af24c2a0564a6183241ea58150
tobacoo_smoke <1879475030@qq.com>
2024-04-22 19:47:08 +0800

    modify Config file

--- src/config/Config.ts
+++ src/config/Config.ts
@@ -1,9 +1,95 @@
 import { DataSourceOptions } from "lakutata/orm";
 import { DataBaseConfig } from "./DataBaseConfig";
 import { DatabaseType } from "../lib/enum/DataBaseType";
-export class Configuration{    
+import { ApplicationOptions } from "lakutata";
+import { BuildEntrypoints, BuildHTTPEntrypoint, HTTPContext } from "lakutata/com/entrypoint";
+import { TestController } from "../controllers/TestController";
+import { TestOrmComponent } from '../components/TestOrmComponent'
+import Fastify from 'fastify'
+import { As } from 'lakutata/helper'
 
-    public dataBaseConfig(isProd:boolean=true,type:DatabaseType=DatabaseType.MYSQL):DataSourceOptions{
-        return DataBaseConfig(isProd,type)
+
+export class Configuration {
+
+
+    protected options: {
+        id: string;
+        /**
+         * AppName
+         */
+        name: string;
+        /**
+         * Application timezone
+         */
+        timezone?: string | 'auto';
+        /**
+         * runtime environment (development or production, default value is development)
+         */
+        mode?: 'development' | 'production';
+    }
+
+    constructor(isProd: boolean = false) {
+        this.options = {
+            id: 'test.app',
+            name: 'test.app',
+            mode: isProd ? 'production' : 'development',
+            timezone: 'auto'
+        }
+    }
+
+    public config(): ApplicationOptions {
+        return {
+            id: this.options.id,
+            name: this.options.name,
+            timezone: this.options.timezone,
+            components: {
+                entrypoint: BuildEntrypoints({
+                    controllers: [
+                        TestController
+                    ],
+                    http: BuildHTTPEntrypoint((module, routeMap, handler, onDestroy) => {
+                        const fastify = Fastify({
+                            logger: false
+                        })
+                        routeMap.forEach((methods: Set<any>, route: string) => {
+                            methods.forEach(method => {
+                                fastify.route({
+                                    url: route,
+                                    method: method,
+                                    handler: async (request, reply) => {
+                                        const ac = new AbortController()
+                                        reply.raw.on('close', () => {
+                                            console.log('close')
+                                            ac.abort()
+                                        })
+                                        return await handler(new HTTPContext({
+                                            route: request.routeOptions.url!,
+                                            method: request.method,
+                                            request: request.raw,
+                                            response: reply.raw,
+                                            data: { ...As<Record<string, string>>(request.query ? request.query : {}), ...As<Record<string, string>>(request.body ? request.body : {}) }
+                                        }), ac)
+                                    }
+                                })
+                            })
+                        })
+                        fastify.listen({ port: 3000, host: '0.0.0.0' })
+                        onDestroy(async () => {
+                            await fastify.close()
+                        })
+                    })
+                }),
+            },
+            bootstrap: [
+                // 'testModule',
+                // 'testComponent',
+                // 'testProvider',
+                'entrypoint'
+            ]
+        }
+    }
+
+    public dataBaseConfig(isProd: boolean = true, type: DatabaseType = DatabaseType.MYSQL): DataSourceOptions {
+        return DataBaseConfig(isProd, type)
     }
 }
--- src/tests/App.spec.ts
+++ src/tests/App.spec.ts
@@ -1,62 +1,11 @@
-import {Application} from 'lakutata'
-import { TestOrmComponent } from '../components/TestOrmComponent'
-import {Configuration} from '../config/Config'
-import { BuildEntrypoints, BuildHTTPEntrypoint, HTTPContext } from 'lakutata/com/entrypoint'
-import { TestController } from '../controllers/TestController'
-import Fastify from 'fastify'
-import { As } from 'lakutata/helper'
+import { Application } from 'lakutata'
+import { Configuration } from '../config/Config'
+
+const configuration = new Configuration()
 
 Application
-    .env({TEST: '123'})
-    .run(() => ({
-        id: 'test.app',
-        name: 'TestApp',
-        timezone: 'auto',
-        components:{
-            entrypoint: BuildEntrypoints({
-                controllers: [
-                    TestController
-                ],
-                http: BuildHTTPEntrypoint((module, routeMap, handler, onDestroy) => {
-                    const fastify = Fastify({
-                        logger: false
-                    })
-                    routeMap.forEach((methods: Set<any>, route: string) => {
-                        methods.forEach(method => {
-                            fastify.route({
-                                url: route,
-                                method: method,
-                                handler: async (request, reply) => {
-                                    const ac = new AbortController()
-                                    reply.raw.on('close', () => {
-                                        console.log('close')
-                                        ac.abort()
-                                    })
-                                    return await handler(new HTTPContext({
-                                        route: request.routeOptions.url!,
-                                        method: request.method,
-                                        request: request.raw,
-                                        response: reply.raw,
-                                        data: {...As<Record<string, string>>(request.query ? request.query : {}), ...As<Record<string, string>>(request.body ? request.body : {})}
-                                    }), ac)
-                                }
-                            })
-                        })
-                    })
-                    fastify.listen({port: 3000, host: '0.0.0.0'})
-                    onDestroy(async () => {
-                        await fastify.close()
-                    })
-                })
-            }),
-        },
-        bootstrap: [
-            // 'testModule',
-            // 'testComponent',
-            // 'testProvider',
-            'entrypoint'
-        ]
-    }))
+    .env({ TEST: '123' })
+    .run(configuration.config())
     .alias({
     }, true)
     .onLaunched(async (app, log) => {
